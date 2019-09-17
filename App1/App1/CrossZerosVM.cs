2fd9f16313e6228de9683f8f83cbfe5809e34a77
Espi4U <hingspeedusa369@gmail.com>
2019-09-17 16:37:39 +0300

    ff

---
+++ App1/App1/CrossZerosVM.cs
@@ -0,0 +1,355 @@
+﻿using Acr.UserDialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App1
+{
+    class CrossZerosVM : Page, INotifyPropertyChanged
+    {
+        public CrossZerosVM()
+        {
+            press = new Command(PressButton);
+            reset = new Command(Reset);
+        }
+        public ICommand press { get; }
+        public ICommand reset { get; }
+        private string _b_00;
+        public string B_00
+        {
+            get => _b_00;
+            set
+            {
+                _b_00 = value;
+                OnPropertyChanged("B_00");
+            }
+        }
+        private string _b_01;
+        public string B_01
+        {
+            get => _b_01;
+            set
+            {
+                _b_01 = value;
+                OnPropertyChanged("B_01");
+            }
+        }
+        private string _b_02;
+        public string B_02
+        {
+            get => _b_02;
+            set
+            {
+                _b_02 = value;
+                OnPropertyChanged("B_02");
+            }
+        }
+        private string _b_10;
+        public string B_10
+        {
+            get => _b_10;
+            set
+            {
+                _b_10 = value;
+                OnPropertyChanged("B_10");
+            }
+        }
+        private string _b_11;
+        public string B_11
+        {
+            get => _b_11;
+            set
+            {
+                _b_11 = value;
+                OnPropertyChanged("B_11");
+            }
+        }
+        private string _b_12;
+        public string B_12
+        {
+            get => _b_12;
+            set
+            {
+                _b_12 = value;
+                OnPropertyChanged("B_12");
+            }
+        }
+        private string _b_20;
+        public string B_20
+        {
+            get => _b_20;
+            set
+            {
+                _b_20 = value;
+                OnPropertyChanged("B_20");
+            }
+        }
+        private string _b_21;
+        public string B_21
+        {
+            get => _b_21;
+            set
+            {
+                _b_21 = value;
+                OnPropertyChanged("B_21");
+            }
+        }
+        private string _b_22;
+        public string B_22
+        {
+            get => _b_22;
+            set
+            {
+                _b_22 = value;
+                OnPropertyChanged("B_22");
+            }
+        }
+        public void PressButton(object sender)
+        {
+            string but = sender as string;
+            bool isChoise = false;
+            while (isChoise == false)
+            {
+                switch (but)
+                {
+                    case "_00":
+                        if (string.IsNullOrEmpty(B_00))
+                        {
+                            B_00 = "X";
+                            isChoise = true;
+                        }
+                        break;
+                    case "_01":
+                        if (string.IsNullOrEmpty(B_01))
+                        {
+                            B_01 = "X";
+                            isChoise = true;
+                        }
+                        break;
+                    case "_02":
+                        if (string.IsNullOrEmpty(B_02))
+                        {
+                            B_02 = "X";
+                            isChoise = true;
+                        }
+                        break;
+                    case "_10":
+                        if (string.IsNullOrEmpty(B_10))
+                        {
+                            B_10 = "X";
+                            isChoise = true;
+                        }
+                        break;
+                    case "_11":
+                        if (string.IsNullOrEmpty(B_11))
+                        {
+                            B_11 = "X";
+                            isChoise = true;
+                        }
+                        break;
+                    case "_12":
+                        if (string.IsNullOrEmpty(B_12))
+                        {
+                            B_12 = "X";
+                            isChoise = true;
+                        }
+                        break;
+                    case "_20":
+                        if (string.IsNullOrEmpty(B_20))
+                        {
+                            B_20 = "X";
+                            isChoise = true;
+                        }
+                        break;
+                    case "_21":
+                        if (string.IsNullOrEmpty(B_21))
+                        {
+                            B_21 = "X";
+                            isChoise = true;
+                        }
+                        break;
+                    case "_22":
+                        if (string.IsNullOrEmpty(B_22))
+                        {
+                            B_22 = "X";
+                            isChoise = true;
+                        }
+                        break;
+                }
+            }
+            if (string.IsNullOrEmpty(B_00) || string.IsNullOrEmpty(B_01) || string.IsNullOrEmpty(B_02) ||
+                string.IsNullOrEmpty(B_10) || string.IsNullOrEmpty(B_11) || string.IsNullOrEmpty(B_12) ||
+                string.IsNullOrEmpty(B_20) || string.IsNullOrEmpty(B_21) || string.IsNullOrEmpty(B_22))
+            {
+                SetRandomZero();
+            }
+            else
+            {
+                SearchWinner();
+            }
+
+        }
+        public void Reset()
+        {
+            B_00 = ""; B_01 = ""; B_02 = "";
+            B_10 = ""; B_11 = ""; B_12 = "";
+            B_20 = ""; B_21 = ""; B_22 = "";
+        }
+        public void SearchWinner()
+        {
+            string[] colums =
+            {
+                string.Format(B_00+B_10+B_20),
+                string.Format(B_01+B_11+B_21),
+                string.Format(B_02+B_12+B_22),
+            };
+            string[] rows =
+            {
+                string.Format(B_00+B_01+B_02),
+                string.Format(B_10+B_11+B_12),
+                string.Format(B_20+B_21+B_22)
+            };
+            string[] diags =
+            {
+                string.Format(B_00+B_11+B_22),
+                string.Format(B_02+B_11+B_20),
+                string.Format("1")
+            };
+            for (int i = 0; i <= 2; i++)
+            {
+                if (rows[i].Where(x => x == 'X').Count() == 3)
+                {
+                    UserDialogs.Instance.Alert("Переможець Х", "Кінець гри");
+                    Reset();
+                }
+                else if (rows[i].Where(x => x == 'O').Count() == 3)
+                {
+                    UserDialogs.Instance.Alert("Переможець О", "Кінець гри");
+                    Reset();
+                }
+                if (colums[i].Where(x => x == 'X').Count() == 3)
+                {
+                    UserDialogs.Instance.Alert("Переможець Х", "Кінець гри");
+                    Reset();
+                }
+                else if (colums[i].Where(x => x == 'O').Count() == 3)
+                {
+                    UserDialogs.Instance.Alert("Переможець О", "Кінець гри");
+                    Reset();
+                }
+                if (diags[i].Where(x => x == 'X').Count() == 3)
+                {
+                    UserDialogs.Instance.Alert("Переможець Х", "Кінець гри");
+                    Reset();
+                }
+                else if (diags[i].Where(x => x == 'O').Count() == 3)
+                {
+                    UserDialogs.Instance.Alert("Переможець О", "Кінець гри");
+                    Reset();
+                }
+            }
+        }
+        public async void SetRandomZero()
+        {
+            Random r = new Random();
+            bool isZero = false;
+            await Task.Run(() =>
+            {
+                UserDialogs.Instance.ShowLoading("Loading", MaskType.Black);
+            });
+            await Task.Delay(3000);
+            while (!isZero)
+            {
+                int point = r.Next(0, 8);
+                switch (point)
+                {
+                    case 0:
+                        if (string.IsNullOrEmpty(B_00))
+                        {
+                            B_00 = "O";
+                            isZero = true;
+                            UserDialogs.Instance.HideLoading();
+                        }
+                        break;
+                    case 1:
+                        if (string.IsNullOrEmpty(B_01))
+                        {
+                            B_01 = "O";
+                            isZero = true;
+                            UserDialogs.Instance.HideLoading();
+                        }
+                        break;
+                    case 2:
+                        if (string.IsNullOrEmpty(B_02))
+                        {
+                            B_02 = "O";
+                            isZero = true;
+                            UserDialogs.Instance.HideLoading();
+                        }
+                        break;
+                    case 3:
+                        if (string.IsNullOrEmpty(B_10))
+                        {
+                            B_10 = "O";
+                            isZero = true;
+                            UserDialogs.Instance.HideLoading();
+                        }
+                        break;
+                    case 4:
+                        if (string.IsNullOrEmpty(B_11))
+                        {
+                            B_11 = "O";
+                            isZero = true;
+                            UserDialogs.Instance.HideLoading();
+                        }
+                        break;
+                    case 5:
+                        if (string.IsNullOrEmpty(B_12))
+                        {
+                            B_12 = "O";
+                            isZero = true;
+                            UserDialogs.Instance.HideLoading();
+                        }
+                        break;
+                    case 6:
+                        if (string.IsNullOrEmpty(B_20))
+                        {
+                            B_20 = "O";
+                            isZero = true;
+                            UserDialogs.Instance.HideLoading();
+                        }
+                        break;
+                    case 7:
+                        if (string.IsNullOrEmpty(B_21))
+                        {
+                            B_21 = "O";
+                            isZero = true;
+                            UserDialogs.Instance.HideLoading();
+                        }
+                        break;
+                    case 8:
+                        if (string.IsNullOrEmpty(B_22))
+                        {
+                            B_22 = "O";
+                            isZero = true;
+                            UserDialogs.Instance.HideLoading();
+                        }
+                        break;
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
